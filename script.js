73cce315690f648ed245c7a4049a6e6a29f9398e
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2023-12-10 12:45:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function fibonacci(num) {
-// your code here
+    if (num === 0) {
+        return 0;
+    } else if (num === 1) {
+        return 1;
+    } else {
+        let a = 0, b = 1;
+        for (let i = 2; i < num; i++) {
+            let temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
 }
 
-module.exports = fibonacci;
+// Test cases
+console.log(fibonacci(1));  // Output: 0
+console.log(fibonacci(5));  // Output: 3
