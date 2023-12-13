012c7307536b5a49d9cec64c3472c7d8d4a44984
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2023-12-13 12:50:15 +0530

    created a submission

---
+++ script.js
@@ -0,0 +1,17 @@
+function fibonacci(num){
+	if(num === 0 || num === 1){
+		return 0 ;
+	}
+	 else{
+		 let a = 0 , b = 1 ;
+		 
+		 for(let n = 2 ; n < num ; n++){
+			 let temp = b ;
+			 b = a + b;
+			 a = temp ;
+		 }
+		 return b ;
+	 }
+}
+
+console.log(fibonacci(1))
